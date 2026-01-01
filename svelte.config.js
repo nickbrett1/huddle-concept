847954eb76577f504a63aa04eacf0ad7aa54e92a
Nick Brett <nick.brett1@gmail.com>
2026-01-01 15:50:00 -0500

    Initial commit: Generated project with 13 capabilities

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-cloudflare is configured for Wrangler deployment
-		// See https://kit.svelte.dev/docs/adapter-cloudflare for more information.
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
