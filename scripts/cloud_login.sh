dd3cb9c5c5e3a6209869103a27e7a05e11325a13
Nick Brett <nick.brett1@gmail.com>
2026-01-02 17:51:32 -0500

    Initial commit: Generated project with 14 capabilities

--- scripts/cloud_login.sh
+++ scripts/cloud_login.sh
@@ -28,6 +28,8 @@ fi
 script -q -c "npx wrangler login --browser=false --callback-host=0.0.0.0 --callback-port=8976 | stdbuf -oL sed 's/0\\\\.0\\\\.0\\\\.0/localhost/g'" /dev/null
 
 
+
+
 echo
 # Setup Wrangler configuration with environment variables
 echo "Setting up Wrangler configuration..."
